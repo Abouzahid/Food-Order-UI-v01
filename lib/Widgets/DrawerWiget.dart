32743568396ba6af4a5b5adb5552de327a442b28
ABOUZAHID ABDELFETTAH <ABOUZAHID.ABDELFETTAH@ofppt-edu.ma>
2023-08-24 12:17:10 +0100

    create drawer

--- lib/Widgets/DrawerWiget.dart
+++ lib/Widgets/DrawerWiget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class  DrawerWiget extends StatelessWidget {
@@ -5,6 +6,112 @@ class  DrawerWiget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            padding: EdgeInsets.zero,
+            child:
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              accountName: Text(
+                "Abdelfettah Abouzahid",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              accountEmail: Text("Abouzahid@gmail.com",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              currentAccountPicture:CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/avatar.jpg"),
+              ) ,
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              CupertinoIcons.home,
+              color: Colors.red,
+            ),
+            title: Text(
+                "Home",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              CupertinoIcons.person,
+              color: Colors.red,
+            ),
+            title: Text(
+              "My Account",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              CupertinoIcons.cart_fill,
+              color: Colors.red,
+            ),
+            title: Text(
+              "My order",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              CupertinoIcons.heart_fill,
+              color: Colors.red,
+            ),
+            title: Text(
+              "My Wish List",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              CupertinoIcons.settings,
+              color: Colors.red,
+            ),
+            title: Text(
+              "Setting",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.exit_to_app,
+              color: Colors.red,
+            ),
+            title: Text(
+              "Log Out",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+      ],),
+    );
   }
 }
